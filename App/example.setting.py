a2769e2e8435f761dd4d8df6a593af22fb2cdfe8
Hidove <i@abcyun.cc>
2020-08-08 13:36:51 +0800

    修复jwt认证问题、升级uvicorn包

--- App/example.setting.py
+++ App/example.setting.py
@@ -9,7 +9,7 @@ class Setting(BaseSettings):
     HOST_MAIL: str = 'mail@qq.com'
     # cloudflare partner 秘钥
     HOST_KEY: str = 'auth_token'
-    SECRET_KEY = os.urandom(24)
+    SECRET_KEY = os.urandom(24).hex()
     ALGORITHM = "HS256"
     # access_token 过期时间
     ACCESS_TOKEN_EXPIRE_MINUTES = 86400
